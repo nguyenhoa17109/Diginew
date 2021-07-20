2bc2fbf2260cf8bfb677220da5e5f817d7485d2a
Hoa <nguyenhoa17109@gmail.com>
2021-07-20 22:02:39 +0700

    commit main content

--- app/src/main/java/com/nguyenhoa/diginew/MainActivity.java
+++ app/src/main/java/com/nguyenhoa/diginew/MainActivity.java
@@ -1,14 +1,44 @@
 package com.nguyenhoa.diginew;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView navigation;
+    private ViewPager viewPager;
+    private NavAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigation = findViewById(R.id.nav);
+        viewPager = findViewById(R.id.viewpager);
+
+        adapter = new NavAdapter(getSupportFragmentManager(),
+                NavAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(adapter);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.mHome: viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.mVideo: viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.mDiscover: viewPager.setCurrentItem(2);
+                        break;
+                    case R.id.mCategories: viewPager.setCurrentItem(3);
+                }
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/nguyenhoa/diginew/NavAdapter.java
@@ -0,0 +1,37 @@
+package com.nguyenhoa.diginew;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.nguyenhoa.diginew.categories.CategoriesFragment;
+import com.nguyenhoa.diginew.discover.DiscoverFragment;
+import com.nguyenhoa.diginew.home.HomeFragment;
+import com.nguyenhoa.diginew.video.VideoFragment;
+
+public class NavAdapter extends FragmentStatePagerAdapter {
+    private int numPage = 4;
+    public NavAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: return new HomeFragment();
+            case 1: return new VideoFragment();
+            case 2: return new DiscoverFragment();
+            case 3: return new CategoriesFragment();
+            default: return new HomeFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numPage;
+    }
+
+
+}
