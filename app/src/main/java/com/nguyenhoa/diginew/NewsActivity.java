bdf0cfcddaa62cbe874edd25018889a6ce48fca8
nguyenhoa17109 <nguyenhoa17109@gmail.com>
2021-07-26 17:32:32 +0700

    update news_activity

--- app/src/main/java/com/nguyenhoa/diginew/NewsActivity.java
+++ app/src/main/java/com/nguyenhoa/diginew/NewsActivity.java
@@ -3,15 +3,26 @@ package com.nguyenhoa.diginew;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,11 +31,17 @@ import com.nguyenhoa.diginew.model.News;
 
 import java.util.ArrayList;
 
+import static com.nguyenhoa.diginew.R.drawable.background_button;
+import static com.nguyenhoa.diginew.R.drawable.background_disable_button;
+
 public class NewsActivity extends AppCompatActivity implements NewsRCAdapter.ItemNewsRCClickListener{
     private TextView tvTitleNews, tvSource, tvTime, tvTopic, tvLikes, tvCmts, tvContent;
     private ImageView ivAccount, ivShare, ivBack;
     private RecyclerView recyclerView;
     private NewsRCAdapter adapter;
+    private News news;
+    private SharedPreferences preferences;
+    public static final String MyPREFERENCES = "CONTENT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +57,7 @@ public class NewsActivity extends AppCompatActivity implements NewsRCAdapter.Ite
         init();
 
         Intent intent = getIntent();
-        News news = (News) intent.getSerializableExtra("News");
+        news = (News) intent.getSerializableExtra("News");
         if(news != null){
             tvTopic.setText(news.getTopic());
             tvTitleNews.setText(news.getTitle());
@@ -50,6 +67,11 @@ public class NewsActivity extends AppCompatActivity implements NewsRCAdapter.Ite
             tvLikes.setText(String.valueOf(news.getLikes()));
             tvCmts.setText(String.valueOf(news.getCmts()));
 
+            int size = preferences.getInt("size", 18);
+            int id = preferences.getInt("font", R.font.roboto);
+            Typeface typeface = ResourcesCompat.getFont(NewsActivity.this, id);
+            tvContent.setTypeface(typeface);
+            tvContent.setTextSize(size);
 
         }else{
             Toast.makeText(this, "LOLO", Toast.LENGTH_SHORT).show();
@@ -68,21 +90,141 @@ public class NewsActivity extends AppCompatActivity implements NewsRCAdapter.Ite
         switch (item.getItemId()){
             case R.id.aRestore:{
                 Toast.makeText(this, "Đã lưu tin", Toast.LENGTH_SHORT).show();
-                return true;
+                break;
             }
             case R.id.aDownload:{
                 Toast.makeText(this, "Đã tải tin", Toast.LENGTH_SHORT).show();
-                return true;
+                break;
             }
             case R.id.aFormatSize:{
-                return true;
+                changeSize();
+                break;
             }
 
         }
         return super.onOptionsItemSelected(item);
     }
 
+    private void changeSize() {
+        final int[] level = new int[1];
+        View view = getLayoutInflater().inflate(R.layout.layout_changesize, null);
+        SeekBar seekBar = view.findViewById(R.id.seekBar);
+        Button bt1, bt2;
+        bt1 = view.findViewById(R.id.bt1);
+        bt2 = view.findViewById(R.id.bt2);
+        seekBar.setProgress(0);
+
+        Dialog dialog = new Dialog(this, R.style.MaterialDialogSheet);
+        dialog.setContentView(view);
+        dialog.setCancelable(true);
+        dialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+        dialog.show();
+
+        int size = preferences.getInt("size", 18);
+        int id = preferences.getInt("font", R.font.lora);
+        switch (size){
+            case 18:
+                seekBar.setProgress(0);
+                break;
+            case 24:
+                seekBar.setProgress(1);
+                break;
+            case 30:
+                seekBar.setProgress(2);
+                break;
+        }
+        Log.d("LOO", R.font.lora+"//"+id);
+        if(id == R.font.lora) {
+            bt2.setBackgroundResource(background_button);
+            bt1.setBackgroundResource(background_disable_button);
+        }else{
+            bt1.setBackgroundResource(background_button);
+            bt2.setBackgroundResource(background_disable_button);
+        }
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                level[0] = seekBar.getProgress();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        bt2.setBackgroundResource(background_button);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bt1.setBackgroundResource(background_button);
+                bt2.setBackgroundResource(background_disable_button);
+
+                int size = 18;
+                switch (level[0]){
+                    case 0:
+                        size = 18;
+                        break;
+                    case 1:
+                        size = 24;
+                        break;
+                    case 2:
+                        size = 30;
+                        break;
+                }
+                tvContent.setTextSize(size);
+//                Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/roboto.ttf");
+                Typeface typeface = ResourcesCompat.getFont(NewsActivity.this, R.font.roboto);
+                tvContent.setTypeface(typeface);
+
+                updateText(size, R.font.roboto);
+                dialog.dismiss();
+            }
+        });
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bt2.setBackgroundResource(background_button);
+                bt1.setBackgroundResource(background_disable_button);
+
+                int size = 18;
+                switch (level[0]){
+                    case 0:
+                        size = 18;
+                        break;
+                    case 1:
+                        size = 24;
+                        break;
+                    case 2:
+                        size = 30;
+                        break;
+                }
+
+                tvContent.setTextSize(size);
+
+//                Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/lora.ttf");
+                Typeface typeface = ResourcesCompat.getFont(NewsActivity.this, R.font.lora);
+                tvContent.setTypeface(typeface);
+
+                updateText(size, R.font.roboto);
+
+                dialog.dismiss();
+            }
+        });
+
+    }
+
     public void init(){
+        preferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
         tvTitleNews = findViewById(R.id.tvTitleNews);
         tvSource = findViewById(R.id.tvNewsSource);
         tvTime = findViewById(R.id.tvNewsTime);
@@ -108,7 +250,7 @@ public class NewsActivity extends AppCompatActivity implements NewsRCAdapter.Ite
 
     private ArrayList<News> setList(){
         ArrayList<News> listNews = new ArrayList<>();
-        listNews.add(new News("Thoi su", "text", "Vietnamnet", 6,
+        listNews.add(new News("Tin tuc", "text", "Vietnamnet", 6,
                 "Hon 80 tan gao ung ho cho 2 'ATM gao' o Da Nang", 100, 200,
                 R.drawable.img_clip, "abc"));
         listNews.add(new News("Thoi su","text", "Vietnamnet", 6,
@@ -122,5 +264,12 @@ public class NewsActivity extends AppCompatActivity implements NewsRCAdapter.Ite
         Intent intent = new Intent(this, NewsActivity.class);
         intent.putExtra("News", adapter.getItem(position));
         startActivity(intent);
+    }
+
+    private void updateText(int size, int id){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt("size", size);
+        editor.putInt("font", id);
+        editor.apply();
     }
 }
