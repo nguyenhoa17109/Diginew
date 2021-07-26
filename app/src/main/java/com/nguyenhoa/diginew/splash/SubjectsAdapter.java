8adda00f82e29369ee3c60de8cf235e8f26e90ef
456anhck2 <nad37vh@gmail.com>
2021-07-26 09:18:14 +0700

    na category1

--- app/src/main/java/com/nguyenhoa/diginew/splash/SubjectsAdapter.java
+++ app/src/main/java/com/nguyenhoa/diginew/splash/SubjectsAdapter.java
@@ -1,6 +1,7 @@
 package com.nguyenhoa.diginew.splash;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,52 +11,73 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.nguyenhoa.diginew.R;
+import com.nguyenhoa.diginew.model.Subject;
 
-public class SubjectsAdapter extends BaseAdapter {
+import java.util.ArrayList;
+
+public class SubjectsAdapter extends RecyclerView.Adapter<SubjectsAdapter.MyViewHolder> {
     private Context context;
-    private LayoutInflater inflater;
-    private String[] numberSubject;
-    private int[] numberImage;
+    private ArrayList<Subject> data;
 
-    public SubjectsAdapter(@NonNull Context context, String[] numberSubject, int[] numberImage) {
+    public SubjectsAdapter(Context context, ArrayList<Subject> data) {
         this.context = context;
-        this.numberImage = numberImage;
-        this.numberSubject = numberSubject;
-    }
-
-    @Override
-    public int getCount() {
-        return numberSubject.length;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
+        this.data = data;
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        if (inflater == null){
-            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        }
-        if(convertView == null){
-            convertView = inflater.inflate(R.layout.item_subjects, null);
-        }
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.item_subjects, parent, false);
+        MyViewHolder viewHolder = new MyViewHolder(v);
 
-        TextView tvSubject = convertView.findViewById(R.id.tvItemSubjects);
-        ImageView ivSubject = convertView.findViewById(R.id.ivItemSubjects);
-
-        ivSubject.setImageResource(numberImage[position]);
-        tvSubject.setText(numberSubject[position]);
-        return convertView;
+        return viewHolder;
     }
 
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Subject subject = data.get(position);
+
+        holder.tvSubject.setText(subject.getName());
+        holder.ivSubject.setImageResource(subject.getImg());
+
+        if(subject.getSelected()){
+            holder.itemView.setBackgroundColor(Color.BLUE);
+        }
+        else{
+            holder.itemView.setBackgroundColor(Color.TRANSPARENT);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(subject.getSelected()){
+                    holder.itemView.setBackgroundColor(Color.TRANSPARENT);
+                    subject.setSelected(false);
+                }
+                else{
+                    holder.itemView.setBackgroundColor(Color.BLUE);
+                    subject.setSelected(true);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tvSubject;
+        ImageView ivSubject;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvSubject = itemView.findViewById(R.id.tvItemSubjects);
+            ivSubject = itemView.findViewById(R.id.ivItemSubjects);
+        }
+    }
 }
