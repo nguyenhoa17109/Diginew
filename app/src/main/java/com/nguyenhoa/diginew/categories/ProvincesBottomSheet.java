290ef25ba43f4339d76e32101db318dded7987ea
456anhck2 <nad37vh@gmail.com>
2021-07-28 11:27:56 +0700

    na update layout category, province

---
+++ app/src/main/java/com/nguyenhoa/diginew/categories/ProvincesBottomSheet.java
@@ -0,0 +1,55 @@
+package com.nguyenhoa.diginew.categories;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.nguyenhoa.diginew.R;
+import com.nguyenhoa.diginew.model.Province;
+
+import java.util.List;
+
+public class ProvincesBottomSheet extends BottomSheetDialogFragment implements ProvincesAdapter.IClickListener {
+    private List<Province> provinceList;
+    private ProvincesAdapter.IClickListener iClickListener;
+
+    public ProvincesBottomSheet(List<Province> provinceList, ProvincesAdapter.IClickListener iClickListener) {
+        this.provinceList = provinceList;
+        this.iClickListener = iClickListener;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        BottomSheetDialog bottomSheetDialog = (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
+
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.bottom_sheet_provinces, null);
+        bottomSheetDialog.setContentView(view);
+
+        RecyclerView rvProvince = view.findViewById(R.id.rvProvinces);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        rvProvince.setLayoutManager(linearLayoutManager);
+
+        ProvincesAdapter provincesAdapter = new ProvincesAdapter(provinceList, this::clickItem);
+        rvProvince.setAdapter(provincesAdapter);
+        RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
+        rvProvince.addItemDecoration(itemDecoration);
+        provincesAdapter.notifyDataSetChanged();
+
+        return bottomSheetDialog;
+    }
+
+    @Override
+    public void clickItem(Province province) {
+        iClickListener.clickItem(province);
+    }
+}
---
+++ app/src/main/java/com/nguyenhoa/diginew/model/Province.java
@@ -0,0 +1,17 @@
+package com.nguyenhoa.diginew.model;
+
+public class Province {
+    String name;
+
+    public Province(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
