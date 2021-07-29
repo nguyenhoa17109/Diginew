1cbd1dcaa4198294c1af628d5761b11ba5856e19
NQ Thai <thaicechcs99@gmail.com>
2021-07-29 23:17:21 +0700

    C1_Setting

---
+++ app/src/main/java/com/nguyenhoa/diginew/model/User.java
@@ -0,0 +1,10 @@
+package com.nguyenhoa.diginew.model;
+
+public class User {
+    private String username;
+    private String name;
+    private String address;
+    private String dob;
+    private String phoneNum;
+    private String servicePack;
+}
